29b64d71c78724ce35a24d8d9ecbe378ff921d19
Yash Goenka <Yash@walkover.in>
2017-01-10 19:26:09 +0530

    admin panel v1

--- app/adminPanel/adminPanelController.js
+++ app/adminPanel/adminPanelController.js
@@ -7,6 +7,7 @@ adminPanel.controller('adminPanelController', ['$scope','$state','$http', 'toast
 	ak ? loggedIn = true : $state.go('login')
 	
 	$this.title = 'Admin Panel'
+    $scope.showfilters = false
 	$scope.companies = []
     $scope.paginationDetails = {}
     $scope.counts = [
@@ -16,6 +17,73 @@ adminPanel.controller('adminPanelController', ['$scope','$state','$http', 'toast
     ]
     $scope.countPage = 20
     $scope.pageNo = 1
+    sortModel = function (){
+     return this.model = [
+        {
+            name: 'Name',
+            key: 'companyName',
+            show: true,
+            value: null
+        },
+        {
+            name: 'Created By',
+            key: 'createdBy',
+            show: true,
+            value: null
+        },
+        {
+            name: 'Created At',
+            key: 'createdAt',
+            show: false,
+            value: null
+        },
+        {
+            name: 'Mobile No.',
+            key: 'mobileNum',
+            show: false,
+            value: null
+        },
+        {
+            name: 'Expiry Date',
+            key: 'subscriptionExpiringAt',
+            show: true,
+            value: null
+        },
+        {
+            name: 'Last Activity',
+            key: 'lastActivity',
+            show: false,
+            value: null
+        },
+        {
+            name: 'Prim. Account balance',
+            key: 'primaryAccBalance',
+            show: true,
+            value: null
+        },
+        {
+            name: 'Sec. Account balance',
+            key: 'secondaryAccBalance',
+            show: true,
+            value: null
+        },
+        {
+            name: 'Subscription plan',
+            key: 'subscriptionPlan',
+            show: false,
+            value: null
+        },
+        {
+            name: 'Total Api Hits',
+            key: 'apiHits',
+            show: true,
+            value: null
+        },
+
+    ]}
+
+    $scope.sortOptions = new sortModel()
+
     $scope.filters = {
         companyName : "",
         companyUniqueName : "",
@@ -24,16 +92,30 @@ adminPanel.controller('adminPanelController', ['$scope','$state','$http', 'toast
         secondaryBillerBalance :"",
         mobileNo : "",
         subscriptionPlan : "",
-        sort : {"companyName": "DESC", "createdBy": "ASC", "subscriptionExpiringAt":"DESC", "primaryAccBalance": "DESC" , "secondaryAccBalance": "DESC" , "apiHits": "DESC"}
+        sort : {"companyName": "DESC"}
     }
 
-
-
+    $this.enableSorting = function (){
+        sort = {}
+        _.each($scope.sortOptions, function(option){
+            if(option.value != null){
+                if(option.value == true || option.value == 'ASC'){
+                    option.value = 'DESC'
+                }
+                else{
+                    option.value = 'ASC'
+                }
+                sort[option.key] = option.value
+            }
+        })
+        return sort
+    }
 
 	$scope.getCompaniesList = function (){
         this.success = function(res){
             $scope.paginationDetails = res.data.body
             $scope.companies = res.data.body.results
+            //$scope.sortOptions = new sortModel()
         }
 
         this.failure = function(res){
@@ -41,6 +123,7 @@ adminPanel.controller('adminPanelController', ['$scope','$state','$http', 'toast
         }
         url = '/admin/companies'
 		filter =  $scope.filters
+        filter.sort = $this.enableSorting()
         data = {
             params: {
                 page: $scope.pageNo,
@@ -59,6 +142,10 @@ adminPanel.controller('adminPanelController', ['$scope','$state','$http', 'toast
         $scope.pageNo = $scope.pageNo - 1
     }
 	
+    $scope.sortColumn = function(value){
+        $scope.getCompaniesList()
+    }
+
 	$scope.getCompaniesList()
 
 	return $this;
