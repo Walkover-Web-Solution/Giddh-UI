f568965d508dd23b51c5f9516fcb9fdefd5dba18
ambitiousdev111 <devansh@walkover.in>
2017-06-06 13:49:55 +0530

    +devansh | testing html pdf dependecy install issue

--- invoice/api/controllers/invoice.js
+++ invoice/api/controllers/invoice.js
@@ -1,4 +1,4 @@
-// 'use strict';
+	// 'use strict';
 
 // const pug = require('pug');
 // const fs = require('fs');
@@ -127,4 +127,26 @@
 //   return utf8;
 // }
 
+
+ 
+// /*--This JavaScript method for Print command--*/
+//     function PrintDoc() {
+//         var toPrint = document.getElementById('printarea');
+//         var popupWin = window.open('', '_blank', 'width=350,height=150,location=no,left=200px');
+//         popupWin.document.open();
+//         popupWin.document.write('<html><title>::Preview::</title><link rel="stylesheet" type="text/css" href="print.css" /></head><body onload="window.print()">')
+//         popupWin.document.write(toPrint.innerHTML);
+//         popupWin.document.write('</html>');
+//         popupWin.document.close();
+//     }
+// /*--This JavaScript method for Print Preview command--*/
+//     function PrintPreview() {
+//         var toPrint = document.getElementById('printarea');
+//         var popupWin = window.open('', '_blank', 'width=350,height=150,location=no,left=200px');
+//         popupWin.document.open();
+//         popupWin.document.write('<html><title>::Print Preview::</title><link rel="stylesheet" type="text/css" href="Print.css" media="screen"/></head><body">')
+//         popupWin.document.write(toPrint.innerHTML);
+//         popupWin.document.write('</html>');
+//         popupWin.document.close();
+//     }
 // module.exports = invoice;
