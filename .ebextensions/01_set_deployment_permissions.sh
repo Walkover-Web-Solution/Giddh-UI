395cd252c14ef84ba8dd8243eba19f3974e0c1f3
gauravmahawar <gaurav@walkover.in>
2017-06-06 15:27:08 +0530

    +Gaurav|Update script file

--- .ebextensions/01_set_deployment_permissions.sh
+++ .ebextensions/01_set_deployment_permissions.sh
@@ -1,8 +1,16 @@
 files:
-  "/opt/elasticbeanstalk/hooks/appdeploy/post/01_set_deployment_permissions.sh":
+  "/tmp/01_set_deployment_permissions.sh":
     mode: "000755"
     owner: root
     group: root
     content: |
-      #!/usr/bin/env bash
+      #! /bin/bash
       chown -R nodejs:nodejs /tmp/
+
+container_commands:
+  00_appdeploy_rewrite_hook:
+    command: cp -v /tmp/01_set_deployment_permissions.sh /opt/elasticbeanstalk/hooks/appdeploy/enact
+  01_configdeploy_rewrite_hook:
+    command: cp -v /tmp/01_set_deployment_permissions.sh /opt/elasticbeanstalk/hooks/configdeploy/enact
+  02_rewrite_hook_perms:
+    command: chmod 755 /opt/elasticbeanstalk/hooks/appdeploy/enact/01_set_deployment_permissions.sh /opt/elasticbeanstalk/hooks/configdeploy/enact/01_set_deployment_permissions.sh
